e51ea6d232f6707085f038f87caa52bd98464f8a
Rafael Cabrera <rafaelcabrera@Rafaels-MacBook-Pro.local>
2021-10-10 20:37:19 -0300

    Readme and initial files

---
+++ examples/Button/Button.ino
@@ -0,0 +1,14 @@
+#include <Led.h>        // https://github.com/meirarc/Led.git
+#include <Button.h>     // https://github.com/meirarc/Button.git
+
+Led led_built_in(LED_BUILTIN);
+Button buttom(0);
+
+void setup() {
+}
+
+void loop() {
+
+  if(buttom.isPressed()) led_built_in.on();
+  else led_built_in.off();
+}
